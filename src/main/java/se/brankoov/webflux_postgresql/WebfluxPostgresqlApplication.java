0791bf86154d8053e9217c85a665a07f24839775
Branko <banne_nikolic@hotmail.com>
2025-10-20 17:24:10 +0200

    First commit

---
+++ src/main/java/se/brankoov/webflux_postgresql/WebfluxPostgresqlApplication.java
@@ -0,0 +1,13 @@
+package se.brankoov.webflux_postgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxPostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxPostgresqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/brankoov/webflux_postgresql/WebfluxPostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package se.brankoov.webflux_postgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxPostgresqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
